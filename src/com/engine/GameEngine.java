5775b5b94001560c38d3ff9ab801afaf3b6629c5
kmangub <karlomangubat92@gmail.com>
2021-08-26 11:15:04 -0700

    Integrated navigating engine to Game Engine, AB#39

--- src/com/engine/GameEngine.java
+++ src/com/engine/GameEngine.java
@@ -1,17 +1,29 @@
 package com.engine;
 
 
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Map;
 
 public class GameEngine {
     boolean winStatus = false;
     boolean loseStatus = false;
-    String currentLocation = "Loading Dock";
+    String currentLocation = "landing dock";
     //Player player;
     //NPC npcs;
     //NPC zombies; ?? later for tracking how many are alive and where?
     //Location locations;
     //Item items;
+
+    // Setting up the different rooms in the spaceship
+    HashMap<String, HashMap<String, String>> spaceship = new HashMap<String, HashMap<String, String>>();
+
+    //Create a landing dock room
+    HashMap<String, String> landingDock = new HashMap<>();
+
+    //Create a hall room
+    HashMap<String, String> hall = new HashMap<>();
 
     public GameEngine() {
         // Set up Player
@@ -24,12 +36,27 @@ public class GameEngine {
 
         // Get Items
         // items = new Item();
+
+
+
+        //Create a door to go north
+        landingDock.put("north", "hall");
+
+        //Create a door to go south
+        hall.put("south", "landing dock");
+
+        //Adding the two rooms to the spaceship object
+        spaceship.put("landing dock", landingDock);
+        spaceship.put("hall", hall);
     }
 
     public void runGameLoop() {
 
         // Start loop
         while (!winStatus && !loseStatus) {
+            //show status
+            showStatus(currentLocation);
+
             // get user input
             Scanner sc = new Scanner(System.in);
             String input = sc.nextLine();
@@ -51,7 +78,33 @@ public class GameEngine {
                     System.out.println("You're hitting.");
                     break;
                 case "go":
-                    System.out.println("You're going.");
+                    if (command[1].equals("north")){
+                        if (spaceship.get(currentLocation).containsKey("north"))
+                            currentLocation = spaceship.get(currentLocation).get("north");
+                        else
+                            System.out.println("Cannot go north");
+                    }
+
+                    if (command[1].equals("south")){
+                        if (spaceship.get(currentLocation).containsKey("south"))
+                            currentLocation = spaceship.get(currentLocation).get("south");
+                        else
+                            System.out.println("Cannot go south");
+                    }
+
+                    if (command[1].equals("east")){
+                        if (spaceship.get(currentLocation).containsKey("east"))
+                            currentLocation = spaceship.get(currentLocation).get("east");
+                        else
+                            System.out.println("Cannot go east");
+                    }
+
+                    if (command[1].equals("west")){
+                        if (spaceship.get(currentLocation).containsKey("west"))
+                            currentLocation = spaceship.get(currentLocation).get("west");
+                        else
+                            System.out.println("Cannot go west");
+                    }
                     break;
                 case "get":
                     System.out.println("You're getting.");
@@ -76,6 +129,19 @@ public class GameEngine {
                 break;
             }
         }
+    }
+
+    public static void showStatus(String location) {
+        System.out.println("You are currently in the " + location);
+        System.out.println();
+        System.out.println("Where do you want to go?");
+        System.out.println("Commands:");
+        System.out.println("go north");
+        System.out.println("go south");
+        System.out.println("go east");
+        System.out.println("go west");
+        System.out.println("q to quit");
+        System.out.println();
     }
 
     private void checkPlayerHealth() {
