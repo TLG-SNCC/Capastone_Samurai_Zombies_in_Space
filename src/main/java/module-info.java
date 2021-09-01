1eb3eab3a7118a5f1c7e552c58301cbe8ea325ca
Joseph Valdez <joseph.m.valdez1@gmail.com>
2021-09-01 10:16:26 -0700

    got latest from dev and resolved dependency conflicts

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module Samurai.Zombies.In.Space {
     requires javafx.controls;
     requires javafx.fxml;
     requires json.simple;
+    requires gson;
+    requires java.sql;
 
     opens com.controller to javafx.fxml;
 
