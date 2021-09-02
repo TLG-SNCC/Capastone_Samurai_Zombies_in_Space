e32cf2f32862539432995f628494644d579be8d4
KL Grady <klgrady@gmail.com>
2021-09-02 14:36:02 -0700

    Smoothed out details and commands to navigate story and station and make commands easier to issue.

--- src/main/java/com/character/NPC.java
+++ src/main/java/com/character/NPC.java
@@ -5,13 +5,16 @@ import java.util.Map;
 
 public class NPC {
 
-    static Map<String, Map<String, ArrayList<String>>> characters = Cast.CAST.getCast();
-    ArrayList<String> dialogue;
-    static int dialogueNum = 0;
+    private static Map<String, Map<String, ArrayList<String>>> characters = Cast.CAST.getCast();
+    private ArrayList<String> dialogue;
+    private String description = "Can't really tell what you're seeing. Are you losing it?";
+    private static int dialogueNum = 0;
 
     public NPC(String character) {
         if (characters.containsKey(character)) {
             dialogue = characters.get(character).get("dialogue");
+            System.out.println(characters.get(character));
+            description = characters.get(character).get("description").get(0);
         } else {
             dialogue = new ArrayList<>();
             dialogue.add("Are you talking to yourself? Could you be infected??");
@@ -19,6 +22,14 @@ public class NPC {
             dialogue.add("There is no response. Is anyone even here?");
         }
 
+    }
+
+    public static boolean checkCast(String potentialNPC) {
+        return characters.containsKey(potentialNPC);
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public String getDialogue() {
